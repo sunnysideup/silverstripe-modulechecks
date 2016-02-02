32d887576b7ddbfc352b90c53022dcef7dd982b6
Sunny Side Up <github@sunnysideup.co.nz>
2016-02-02 13:13:05 +1300

    CLEANUP

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# silverstripe-module-management
+## Silverstripe Module Checks ##
+
+Allows you to check a bunch of
+very basic requirements for your Silverstripe Modules
+such as whether it is listed on packagist.
+
+## Installation Instructions ##
+
+Install like any
+other Silverstripe Module module.
+
+To use the module, set the configs in the task and
+then browse to /dev/tasks/ModuleChecks to run ...
+
+## Developers ##
+
+Nicolaas Francken [at] sunnysideup.co.nz
---
+++ code/ModuleChecks.php
@@ -0,0 +1,168 @@
+<?php
+
+
+class ModuleChecks extends buildtask {
+
+	private static $git_user_name = "sunnysideup";
+
+	private static $packagist_user_name = "sunnysideup";
+
+	private static $modules = array();
+
+	private static $methods_to_check = array(
+		"exitsOnPackagist",
+		"hasReadMeFile",
+		"hasLicense",
+		"hasComposerFile",
+		"existsOnAddOns",
+	);
+
+	protected $title = "Check Modules on Github and Packagist";
+
+	protected $description = "Goes through every module on github and checks for some of the basic requirements. You will need to set your GitHub Username in the configs.";
+
+	function run($request) {
+
+		increase_time_limit_to(3600);
+		$this->getAllRepos();
+		$methodsToCheck = $this->Config()->get("methods_to_check");
+		foreach(self::$modules as $module) {
+			DB::alteration_message("<h3>Checking $module</h3>");
+			foreach($methodsToCheck as $method) {
+				if(!$this->$method($module)) {
+					DB::alteration_message("bad response for $method", "deleted");
+				}
+			}
+			ob_flush();
+			flush();
+		}
+	}
+
+	/**
+	 * @param string $name
+	 * @return boolean
+	 */
+	protected function exitsOnPackagist($name){
+		return $this->checkLocation("https://packagist.org/packages/".$this->Config()->get("packagist_user_name")."/$name");
+	}
+
+
+	/**
+	 * @param string $name
+	 *
+	 * @return boolean
+	 */
+	protected function hasLicense($name){
+		return $this->checkLocation("https://raw.githubusercontent.com/".$this->Config()->get("git_user_name")."/silverstripe-".$name."/master/LICENSE");
+	}
+
+	/**
+	 * @param string $name
+	 *
+	 * @return boolean
+	 */
+	protected function hasComposerFile($name){
+		return $this->checkLocation("https://raw.githubusercontent.com/".$this->Config()->get("git_user_name")."/silverstripe-".$name."/master/composer.json");
+	}
+
+	/**
+	 * @param string $name
+	 *
+	 * @return boolean
+	 */
+	protected function hasReadMeFile($name){
+		return $this->checkLocation("https://raw.githubusercontent.com/".$this->Config()->get("git_user_name")."/silverstripe-".$name."/master/README.md");
+	}
+
+	protected function existsOnAddOns($name) {
+		return $this->checkLocation("http://addons.silverstripe.org/add-ons/".$this->Config()->get("packagist_user_name")."/$name");
+	}
+
+	/**
+	 * takes the preloaded modules and add any other ones you have listed...
+	 */
+	protected function getAllRepos(){
+		$username = $this->Config()->get("git_user_name");
+		$ch = curl_init();
+		// set url
+		curl_setopt($ch, CURLOPT_URL, "https://api.github.com/users/".$username."/repos");
+		//return the transfer as a string
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+		// $output contains the output string
+		$string = curl_exec($ch);
+
+		// close curl resource to free up system resources
+		curl_close($ch);
+		$array = json_decode($string, true);
+		foreach($array as $repo) {
+			//dont bother about forks
+			if(!$repo["fork"]) {
+				//make sure we are the owners
+				if($repo["owner"]["login"] == $username) {
+					//check it is silverstripe module
+					$nameWithoutPrefix = str_replace("silverstripe-", "", $repo["name"]);
+					if(strlen($nameWithoutPrefix) < strlen($repo["name"])) {
+						//is it listed yet?
+						if(!in_array($nameWithoutPrefix, self::$modules)) {
+							self::$modules[] = $nameWithoutPrefix;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	protected function checkForDetailsInComposerFile(){
+		die("to be completed");
+		//check require in composer.json
+		$data = file_get_contents($location);
+		// Is the str in the data (case-insensitive search)
+		if (stripos($data, "\"require\":{") !== false){
+			// sw00t! we have a match
+			echo "<div style='color: green;' class='ok'>OK $location has require</div>";
+		}
+		else {
+			echo "<div style='color: red;'>BAD $location does not have require</div>";
+		}
+
+		//check require in composer.json
+		$data = file_get_contents($location);
+		// Is the str in the data (case-insensitive search)
+		if (stripos($data, "\"extra\":{") !== false){
+			// sw00t! we have a match
+			echo "<div style='color: green;' class='ok'>OK $location has extra</div>";
+		}
+		else {
+			echo "<div style='color: red;'>BAD $location does not have extra</div>";
+		}
+
+		//check authors in composer.json
+		$data = file_get_contents($location);
+		// Is the str in the data (case-insensitive search)
+		if (stripos($data, "\"authors\":[") !== false){
+			// sw00t! we have a match
+			echo "<div style='color: green;' class='ok'>OK $location has authors</div>";
+		}
+		else {
+			echo "<div style='color: red;'>BAD $location does not have authors</div>";
+		}
+	}
+
+
+	protected function checkLocation($url) {
+		$handle = curl_init($url);
+		curl_setopt($handle,  CURLOPT_RETURNTRANSFER, TRUE);
+
+		/* Get the HTML or whatever is linked in $url. */
+		$response = curl_exec($handle);
+
+		/* Check for 404 (file not found). */
+		$httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+		$outcome = $httpCode == 200;
+		curl_close($handle);
+		return $outcome;
+	}
+
+
+}
