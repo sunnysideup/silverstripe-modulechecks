7b044ffb9e27cb298cef8ab988e398d76d062649
Nicolaas <github@sunnysideup.co.nz>
2016-07-07 20:56:29 +1200

    fixes

--- code/tasks/ModuleTasksConfig.php
+++ code/tasks/ModuleTasksConfig.php
@@ -17,26 +17,7 @@ abstract Class ModuleTasksConfig extends BuildTask {
      * A list of module names to process
      * */
     private static $modules = array( 
-        'cms_tricks_for_apps', 
-        'cms_edit_link_field', 
-        'frontendeditor', 
-        'payment_stripe', 
-        'table_filter_sort', 
-        'us_phone_number', 
-        'blog_shared_categorisation', 
-        'comments_add_recaptcha', 
-        'ecommerce_cloud_flare_geoip', 
-        'ecommerce_nutritional_products', 
-        'ecommerce_stockists', 
-        'email_address_database_field', 
-        'import_task', 
-        'pdf_upload_field', 
-        'perfect_cms_images', 
-        'phone_field', 
-        'share_this_simple', 
-        'webpack_requirements_backend', 
-        'search_data_objects', 
-        'translate'
+        
     );
 
     /*
@@ -44,8 +25,8 @@ abstract Class ModuleTasksConfig extends BuildTask {
      */
     private static $default_module_settings = array(
         'readme' => 'readme.md',
-        'localDir' => '/home/jack/modules/',
-        'baseGitHubURL' => "https://github.com/sunnysideup/silverstripe-"
+        'localDir' => '',
+        'baseGitHubURL' => ""
     );
 
 
