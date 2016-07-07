84829b259b762143e8b68422df3e6ec0a337888f
Jack Stinchcombe <sunnysideupdevs@gmail.com>
2016-07-07 12:56:21 +1200

    test commit

---
+++ code/tasks/ModuleTasksConfig.php
@@ -0,0 +1,37 @@
+<?php
+
+Class ModuleTasksConfig {
+
+    public static $modules = array( 
+        'cms_tricks_for_apps', 
+        'cms_edit_link_field', 
+        'frontendeditor', 
+        'payment_stripe', 
+        'table_filter_sort', 
+        'us_phone_number', 
+        'blog_shared_categorisation', 
+        'comments_add_recaptcha', 
+        'ecommerce_cloud_flare_geoip', 
+        'ecommerce_nutritional_products', 
+        'ecommerce_stockists', 
+        'email_address_database_field', 
+        'import_task', 
+        'pdf_upload_field', 
+        'perfect_cms_images', 
+        'phone_field', 
+        'share_this_simple', 
+        'webpack_requirements_backend', 
+        'search_data_objects', 
+        'translate'
+    );
+
+    $local_storage_dir ='/home/jack/modules/';
+
+    public static $default_module_settings = $array(
+        'Readme' => 'readme.md'
+    );
+
+}
+
+
+?>
