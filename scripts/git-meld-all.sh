072a571c98494105d1f948a36d364271be28a9ba
Marcel Nogueira d'Eurydice <mrcl@sunnysideup.co.nz>
2016-11-07 15:22:06 +1300

    MINOR: Update to git-meld-script. Includes base folder.

--- scripts/git-meld-all.sh
+++ scripts/git-meld-all.sh
@@ -1,29 +1,61 @@
+
 #!/bin/bash
 
-folders=`find . -maxdepth 0 -mindepth 1 -type d`
+folders=`find . -mindepth 0 -maxdepth 1 -type d`
 rootFolder=$PWD
 
-for folder in $folders; do
-    if [ -f $folder/.git/config ] ; then
-    if grep --quiet sunnysideup $folder/.git/config; then
-        cd $folder
-        modifiedFiles=`git diff --name-only  HEAD`
-        if [[ $modifiedFiles ]];then
-        echo 'You have modified files in '$folder
-        for f in $modifiedFiles;do
-            echo $f
-        done
-        echo 'Would you like to commit changes?'
-        read commit
+# Defining Colors
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+ORANGE='\033[0;33m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
 
-        if [[ $commit == "y" ]];then
-            git add --all
-            meld .
-        fi
-        echo ""
-        echo "---------------------------------------------------"
-        fi
-        cd ..
-    fi
+# Vendor to look for
+vendor="sunnysideup"
+
+for folder in $folders; do
+    if [ -f $folder/.git/config ] ; then # is a git working copy
+	# Belongs to sunnysideup
+	if ( grep --quiet $vendor $folder/.git/config ) || [[ $folder == "." ]]; then
+	    cd $folder
+	    folderChanges=`git diff --name-only  HEAD`
+	    if [[ $folderChanges ]];then
+		echo -e "-- ${CYAN}You have modified files in $folder${NC}"
+
+		untrackedFiles=`git ls-files --others --exclude-standard`
+		for file in $untrackedFiles;do 
+		    echo -e "--> ${RED}$file${NC}"
+		done
+
+		modifiedFiles=`git ls-files -m`
+		for file in $modifiedFiles;do
+		    echo -e "--> ${YELLOW}$file${NC}"
+		done
+
+		addedFiles=`git diff --name-only --cached`
+		for file in $addedFiles;do
+		    echo -e "--> ${GREEN}$file${NC}"
+		done
+		
+		echo -e "-- ${ORANGE}Would you like to commit changes?${NC} [y/n]"
+		read commit
+
+		if [[ $commit == "y" ]];then
+		    git add --all
+		    meld .
+		fi
+		echo -e "-- ${CYAN}Completed ${BLUE}$folder${NC} --"
+		echo ""
+
+	    fi
+	    cd $rootFolder
+	fi
     fi
 done
+
+echo -e "-- ${CYAN}END${NC} --------------------";
+
+    
