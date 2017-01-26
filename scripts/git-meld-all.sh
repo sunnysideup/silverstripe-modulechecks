ff120cc9a2eef4a0d23fca0b2a7c4ce57887e1b8
Nicolaas <github@sunnysideup.co.nz>
2017-01-26 14:41:50 +1300

    Update git-meld-all.sh

--- scripts/git-meld-all.sh
+++ scripts/git-meld-all.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # how to install?
@@ -5,7 +6,7 @@
 # 2. edit ~/.bashrc to include this line ... export PATH=$PATH:~/local/bin/
 # 3. make sure the git-meld-all file can be executed
 
-# deleting foo.bar.orig files ... 
+# deleting foo.bar.orig files ...
 find . -name "*.orig" -exec rm "{}" -i \;
 
 # find folders
@@ -28,47 +29,47 @@ vendor="sunnysideup"
 
 gitCustomStatus (){
     git status --porcelain | while read status file; do
-	case $status in 
-	    $1) echo -e "$3 $2$1\t$file${NC}";;
-	esac
+    case $status in
+        $1) echo -e "$3 $2$1\t$file${NC}";;
+    esac
     done
 }
 
 for folder in $folders; do
     if [ -f $folder/.git/config ] ; then # is a git working copy
-	# Belongs to sunnysideup
-	if ( grep --quiet $vendor $folder/.git/config ) || [[ $folder == "." ]]; then
-	    cd $folder
-	    folderChanges=`git status --porcelain`
-	    if [[ $folderChanges ]];then
-		echo -e "-- ${CYAN}You have modified files in $folder${NC}"
+    # Belongs to sunnysideup
+    if ( grep --quiet $vendor $folder/.git/config ) || [[ $folder == "." ]]; then
+        cd $folder
+        folderChanges=`git status --porcelain`
+        if [[ $folderChanges ]];then
+        echo -e "-- ${CYAN}You have modified files in $folder${NC}"
 
-		gitCustomStatus M "${YELLOW}" "+++"    # Modified
-		gitCustomStatus A "${GREEN}" "+++"     # Added
-		gitCustomStatus R "${BLUE2}" "+++"     # Renamed
-		gitCustomStatus C "${BLUE}" "+++"      # Copied
-		gitCustomStatus D "${RED}" "---"       # Deleted
-		
-		if [[ `git status --porcelain | grep "^?? "` ]];then
-		    echo -e "\n-- ${CYAN} Untracked files${NC}"
-		    gitCustomStatus2 ?? "${RED2}" "   " # Untracked
-		fi
-		
-		echo -e "\n-- ${ORANGE}Would you like to commit changes?${NC} [y/n]"
-		read commit
+        gitCustomStatus M "${YELLOW}" "+++"    # Modified
+        gitCustomStatus A "${GREEN}" "+++"     # Added
+        gitCustomStatus R "${BLUE2}" "+++"     # Renamed
+        gitCustomStatus C "${BLUE}" "+++"      # Copied
+        gitCustomStatus D "${RED}" "---"       # Deleted
 
-		if [[ $commit == "y" ]];then
-		    git add --all
-		    meld .
-		fi
-        git pull origin $(git rev-parse --abbrev-ref HEAD) 
-        git push origin $(git rev-parse --abbrev-ref HEAD)        
-		echo -e "-- ${CYAN}Completed ${BLUE2}$folder${NC} --"
-		echo ""
+        if [[ `git status --porcelain | grep "^?? "` ]];then
+            echo -e "\n-- ${CYAN} Untracked files${NC}"
+            gitCustomStatus2 ?? "${RED2}" "   " # Untracked
+        fi
 
-	    fi
-	    cd $rootFolder
-	fi
+        echo -e "\n-- ${ORANGE}Would you like to commit changes?${NC} [y/n]"
+        read commit
+
+        if [[ $commit == "y" ]];then
+            git add --all
+            meld .
+            git pull origin "$(git branch | grep -E '^\* ' | sed 's/^\* //g')"
+            git push origin "$(git branch | grep -E '^\* ' | sed 's/^\* //g')"
+        fi
+        echo -e "-- ${CYAN}Completed ${BLUE2}$folder${NC} --"
+        echo ""
+
+        fi
+        cd $rootFolder
+    fi
     fi
 done
 
@@ -82,4 +83,3 @@ for folder in $folders; do
         cd $rootFolder
     fi
 done
-
