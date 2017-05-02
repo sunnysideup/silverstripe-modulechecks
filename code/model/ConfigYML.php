ba1ae37a22543fc34ce808af8e2fdb142eb0f49b
sunnysideup <github@sunnysideup.co.nz>
2017-05-03 11:20:47 +1200

    PATCH: fixes

--- code/model/ConfigYML.php
+++ code/model/ConfigYML.php
@@ -1,5 +1,7 @@
 <?php
-require ('../modulechecks/vendor/autoload.php');
+
+//require ('../modulechecks/vendor/autoload.php');
+
 use Symfony\Component\Yaml\Yaml;
 
 
@@ -14,51 +16,51 @@ Class ConfigYML extends Object {
         $this->yaml_data = null;
         $folder = GitHubModule::Config()->get('absolute_temp_folder');
 
-        $this->filename = $folder . '/' . $this->moduleName . '/_config/config.yml';        
+        $this->filename = $folder . '/' . $this->moduleName . '/_config/config.yml';
     }
-    
+
     public function reWrite(){
-		if (! $this->readYMLFromFile()) 
+		if (! $this->readYMLFromFile())
 		{
 			return false;
 		}
-		if (! $this->writeYAMLToFile()) 
+		if (! $this->writeYAMLToFile())
 		{
 			return false;
 		}
-		return true;		
+		return true;
 	}
 
     public function readYMLFromFile() {
 
 		GeneralMethods::output_to_screen("reading config yml ...  ",'updating');
-		
-		if (! file_exists ($this->filename)) 
+
+		if (! file_exists ($this->filename))
 		{
                     GeneralMethods::output_to_screen("<li>Unable to load: " . $this->filename, 'updated') ;
                     //UpdateModules::$unsolvedItems[$this->gitHubModuleInstance->ModuleName] = "Unable to load " . $this->filename;
-                    
+
                     UpdateModules::addUnsolvedProblem($this->gitHubModuleInstance->ModuleName, "Unable to load " . $this->filename);
                     return false;
 		}
-			
+
         try {
             $this->yaml_data = Yaml::parse(file_get_contents($this->filename));
-            
-            
-            
-            
+
+
+
+
         } catch (Exception $e) {
             GeneralMethods::output_to_screen("<li>Unable to parse the YAML string: " .$e->getMessage(). " <li>", 'updated') ;
-            
+
             //UpdateModules::$unsolvedItems[$this->gitHubModuleInstance->ModuleName] = "Unable to parse the YAML string: " .$e->getMessage();
-            
+
             UpdateModules::addUnsolvedProblem($this->gitHubModuleInstance->ModuleName, "Unable to parse the YAML string: " .$e->getMessage());
-            
+
 			//trigger_error ("Error in YML file");
-	
+
 			$this->replaceFaultyYML();
-	
+
 			return false;
         }
 
@@ -66,24 +68,24 @@ Class ConfigYML extends Object {
         return $this->yaml_data;
 
     }
-    
-    
+
+
     public function replaceFaultyYML() {
-           
+
         return false;
-           
+
 		/**function broken do not use**/
-    
-    
+
+
 		if (file_exists ($this->filename)) {
-			
-			
+
+
 			$rawYML = file_get_contents($this->filename);
-			
+
 			$lines = explode("\n", $rawYML);
-			
+
 			$replacment = '';
-			
+
 			foreach ($lines as $index=>$line) {
 				if (strpos ($line, "After:" ) !==false) {
 					$replacment = "After:";
@@ -94,7 +96,7 @@ Class ConfigYML extends Object {
 						if (! trim ($item)) {
 							continue;
 						}
-						
+
 						$item = str_replace('After: ', '', $item);
 						$replacment .= '  - '. trim($item) . "";
 					}
@@ -102,36 +104,36 @@ Class ConfigYML extends Object {
 				}
 			}
 			$newYML = implode('', $lines);
-			
+
 			GeneralMethods::output_to_screen("Updating config.YML to correct syntax ... ",'updating');
-			
+
 			$file = fopen($this->filename, "w");
-			
-			
-			
+
+
+
 			file_put_contents($this->filename, $newYML);
-			
-			
+
+
 		}
 		else
 		{
 			return false;
 		}
-           
+
     }
     public function writeYAMLToFile() {
-		
+
 		GeneralMethods::output_to_screen("Writing config yml ... ",'updating');
-		
+
 		if (!$this->yaml_data) {
 			return false;
 		}
-		
+
 		$yaml = Yaml::dump($this->yaml_data);
 		file_put_contents($this->filename, $yaml);
 		return true;
-		
-		
+
+
 	}
 
     private function catchFopenWarning() {
