d9f63b08ddc2c381ce575f4bf59c061fd08350eb
Nicolaas <github@sunnysideup.co.nz>
2018-12-07 07:23:32 +1300

    Update git-patch-all.sh

--- scripts/git-patch-all.sh
+++ scripts/git-patch-all.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+# how to install?
+# 1. create ~/local/bin/git-patch-all.sh and copy contents of this file
+# 2. edit ~/.bashrc to include this line ... export PATH=$PATH:~/local/bin/
+# 3. you may need to run: chmod +x ~/local/bin/git-patch-all.sh
+
+# you can now run git-patch-all.sh from the command line.
+
 git add . -A
 git commit . -m "PATCH: automated commit"
 git push
