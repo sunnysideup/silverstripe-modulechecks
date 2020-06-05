ba1c51d9c23897923f76767cc9d5bff1e5a413da
Nicolaas Thiemen <nt@sunnysideup.co.nz>
2020-06-05 17:45:24 +1200

    MAJOR: upgrade to new version of Silverstripe - step: Add the table name

--- code/objects/GitHubModule.php
+++ code/objects/GitHubModule.php
@@ -56,6 +56,20 @@ class GitHubModule extends DataObject
      */
     protected $gitRepo = null;
 
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'GitHubModule';
+
     private static $db = array(
         'ModuleName' => 'VarChar(100)',
         'Description' => 'VarChar(300)'
