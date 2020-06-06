2f66c972beacb1fc5cd82659e9f244b626c1eb39
Nicolaas - Sunny Side Up <nt@sunnysideup.co.nz>
2020-06-06 22:07:07 +1200

    PATCH: cleanup

--- vendor/silverstripe/assets/src/FileMigrationHelper.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use SilverStripe\Assets\Dev\Tasks\FileMigrationHelper;
-
-if (!class_exists('SilverStripe\\Assets\\FileMigrationHelper')) {
-    class_alias(FileMigrationHelper::class, 'SilverStripe\\Assets\\FileMigrationHelper');
-}
